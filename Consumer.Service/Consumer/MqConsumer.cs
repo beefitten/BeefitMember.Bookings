6879ef9a888a375d7a6053d8ad224a3024aaf2e7
nikolajhuynh <nikolajtbh@gmail.com>
2021-10-06 10:49:35 +0200

    Added MessageQueue

--- Consumer.Service/Consumer/MqConsumer.cs
+++ Consumer.Service/Consumer/MqConsumer.cs
@@ -1,23 +1,25 @@
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Consumer.Service.Handlers;
 using Consumer.Service.Handlers.Interfaces;
 using Domain.Events;
+using Microsoft.Extensions.Hosting;
 using Newtonsoft.Json;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
 namespace Consumer.Service.Consumer
 {
-    public class MqConsumer
+    public class MqConsumer : BackgroundService
     {
         private IConnection _connection;
         private IModel _channel;
         private readonly IBookingsHandler _bookingsHandler; 
         
-        public MqConsumer(IBookingsHandler bookingsHandler)
-        {
-            _bookingsHandler = bookingsHandler;
+        public MqConsumer()
+        { 
+            _bookingsHandler = new BookingsHandler();
            setupConnection();
         }
 
@@ -26,65 +28,60 @@ namespace Consumer.Service.Consumer
             var factory = new ConnectionFactory {HostName = "localhost"};
 
             _connection = factory.CreateConnection();
-            // _connection.ConnectionShutdown += RabbitMQ_ShutdownConnection;
             _channel = _connection.CreateModel();
             _channel.QueueDeclare(
-                queue: "classBooking", 
-                durable: false, 
-                exclusive: false, 
+                queue: "classBooking",
+                durable: false,
+                exclusive: false,
                 autoDelete: false,
                 arguments: null);
+            
+            _channel.BasicQos(0, 1, false);
+
+            _connection.ConnectionShutdown += RabbitMQ_ShutdownConnection;
+
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            stoppingToken.ThrowIfCancellationRequested();
+        
             var consumer = new EventingBasicConsumer(_channel);
             consumer.Received += (ch, ea) =>
             {
                 var content = Encoding.UTF8.GetString(ea.Body.ToArray());
                 var received = JsonConvert.DeserializeObject<BookClassEvent>(content);
-
+        
                 HandleMessage(received);
                 _channel.BasicAck(ea.DeliveryTag, false);
             };
+        
+            consumer.Shutdown += ConsumerShutdown;
+            consumer.Registered += ConsumerRegistered;
+            consumer.Unregistered += ConsumerUnregistered;
+            consumer.ConsumerCancelled += ConsumerCancelled;
+            
+            _channel.BasicConsume("classBooking", false, consumer);
+            
+            return Task.CompletedTask;
         }
 
-        // protected override Task ExecuteAsync(CancellationToken stoppingToken)
-        // {
-        //     stoppingToken.ThrowIfCancellationRequested();
-        //
-        //     var consumer = new EventingBasicConsumer(_channel);
-        //     consumer.Received += (ch, ea) =>
-        //     {
-        //         var content = Encoding.UTF8.GetString(ea.Body.ToArray());
-        //         var received = JsonConvert.DeserializeObject<BookClassEvent>(content);
-        //
-        //         HandleMessage(received);
-        //         _channel.BasicAck(ea.DeliveryTag, false);
-        //     };
-        //
-        //     // consumer.Shutdown += ConsumerShutdown;
-        //     // consumer.Registered += ConsumerRegistered;
-        //     // consumer.Unregistered += ConsumerUnregistered;
-        //     // consumer.ConsumerCancelled += ConsumerCancelled;
-        //     
-        //     _channel.BasicConsume("classBooking", false, consumer);
-        //     
-        //     return Task.CompletedTask;
-        // }
-
-        // private void ConsumerCancelled(object? sender, ConsumerEventArgs e)
-        // {
-        // }
-        //
-        // private void ConsumerUnregistered(object? sender, ConsumerEventArgs e)
-        // {
-        // }
-        //
-        // private void ConsumerRegistered(object? sender, ConsumerEventArgs e)
-        // {
-        // }
-        //
-        // private void ConsumerShutdown(object? sender, ShutdownEventArgs e)
-        // {
-        //     
-        // }
+        private void ConsumerCancelled(object? sender, ConsumerEventArgs e)
+        {
+        }
+        
+        private void ConsumerUnregistered(object? sender, ConsumerEventArgs e)
+        {
+        }
+        
+        private void ConsumerRegistered(object? sender, ConsumerEventArgs e)
+        {
+        }
+        
+        private void ConsumerShutdown(object? sender, ShutdownEventArgs e)
+        {
+            
+        }
 
         private void HandleMessage(BookClassEvent evt)
         {
@@ -94,6 +91,13 @@ namespace Consumer.Service.Consumer
         private void RabbitMQ_ShutdownConnection(object sender, ShutdownEventArgs e)
         {
             
+        }
+
+        public override void Dispose()
+        {
+            _channel.Close();
+            _connection.Close();
+            base.Dispose();
         }
     }
 }
--- Consumer.Service/Program.cs
+++ Consumer.Service/Program.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Text;
-using Consumer.Service.Consumer;
-using Consumer.Service.Handlers;
-using RabbitMQ.Client;
-using RabbitMQ.Client.Events;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace Consumer.Service
 {
@@ -11,35 +7,11 @@ namespace Consumer.Service
     {
         static void Main(string[] args)
         {
-            // var bookingsHandler = new BookingsHandler();
-            // var consumer = new MqConsumer(bookingsHandler);
-            //
-            // Console.ReadLine();
-            
-            var factory = new ConnectionFactory() { HostName = "localhost" };
-            using (var connection = factory.CreateConnection())
-            {
-                using (var channel = connection.CreateModel())
-                {
-                    channel.QueueDeclare(queue: "classBooking",
-                        durable: false,
-                        exclusive: false,
-                        autoDelete: false,
-                        arguments: null);
-                    var consumer = new EventingBasicConsumer(channel);
-                    consumer.Received += (model, ea) =>
-                    {
-                        var body = ea.Body.ToArray();
-                        var message = Encoding.UTF8.GetString(body);
-                        Console.WriteLine("Received: {0}", message);
-                    };
-                    channel.BasicConsume(queue: "classBooking",
-                        autoAck: true,
-                        consumer: consumer);
-
-                    Console.ReadLine();
-                }
-            }
+            CreateHostBuilder(args).Build().Run();
         }
+        
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>().UseUrls("http://localhost:3004"); });
     }
 }
